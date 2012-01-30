d81983c14a17c26f2e3a7911e0a01a28e6e839b0
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-30 23:46:59 +0600

    test: cleanup

--- test/test-api.cc
+++ test/test-api.cc
@@ -2,26 +2,12 @@
 
 static const char* adjective = "neat";
 
-class Object : public ObjectTemplate {
+class ArrayObject : public ObjectTemplate {
  public:
-  Object() {}
-  ~Object() {}
+  const char* GetString(const char* key) { return NULL; }
+  ArrayObject* GetObject(const char* key) { return NULL; }
 
-  const char* GetString(const char* key) {
-    assert(strcmp(key, "adjective") == 0);
-
-    return adjective;
-  }
-
-  Object* GetObject(const char* key) {
-    if (strcmp(key, "prop") == 0) {
-      return this;
-    } else {
-      return NULL;
-    }
-  }
-
-  Object* At(const int index) {
+  ArrayObject* At(const int index) {
     if (index < 3) {
       return this;
     } else {
@@ -34,15 +20,56 @@ class Object : public ObjectTemplate {
   }
 };
 
+class Object : public ObjectTemplate {
+ public:
+  const char* GetString(const char* key) {
+    assert(strcmp(key, "adjective") == 0);
+
+    return adjective;
+  }
+
+  ObjectTemplate* GetObject(const char* key) {
+    if (strcmp(key, "prop") == 0) {
+      return this;
+    } if (strcmp(key, "arrprop") == 0) {
+      return &array;
+    } else {
+      return NULL;
+    }
+  }
+
+  Object* At(const int index) {
+    return NULL;
+  }
+
+  bool IsArray() {
+    return false;
+  }
+
+  ArrayObject array;
+};
+
 TEST_START("API test")
-  Template* t = hogan.Compile("some {{adjective}} template. "
-                               "{{#prop}}yeah {{^prop}}oh noes{{/prop}}");
-
   Object data;
+  Template* t;
+  char* out;
 
-  char* out = t->Render(&data);
+  t = hogan.Compile("some {{adjective}} template. "
+                    "{{#prop}}yeah{{^prop}}oh noes{{/prop}}");
+
+  out = t->Render(&data);
   assert(out != NULL);
-  assert(strcmp("some neat template. yeah yeah yeah ", out) == 0);
+  assert(strcmp("some neat template. yeah", out) == 0);
+
+  delete t;
+  delete out;
+
+  t = hogan.Compile("some {{adjective}} template."
+                    "{{#arrprop}} o{{^arrprop}}oh noes{{/arrprop}}");
+
+  out = t->Render(&data);
+  assert(out != NULL);
+  assert(strcmp("some neat template. o o o", out) == 0);
 
   delete t;
   delete out;
